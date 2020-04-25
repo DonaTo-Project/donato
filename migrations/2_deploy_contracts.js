02844b1afda1576b9a7807a2c7fc7fca258a82d3
raphaelpg <raphael_pinto@outlook.com>
2020-04-25 17:38:52 +0200

    updated 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const TokenERC20Dai = artifacts.require("TokenERC20Dai");
 const Donato = artifacts.require("Donato");
-
+//
 module.exports = function(deployer) {
 	deployer.then(async () => {
       await deployer.deploy(TokenERC20Dai);
