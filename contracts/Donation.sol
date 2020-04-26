4681508573c85ce2301b68ef182313a01dac2862
raphaelpg <raphael_pinto@outlook.com>
2020-04-26 12:54:37 +0200

    added natspecs comments to contracts

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -5,6 +5,10 @@ import "./SafeMath.sol";
 import "./DonatoReceiver.sol";
 import "./AdminRole.sol";
 
+///@title Donato
+///@author Lorenzo Bersano and Raphael Pinto
+///@notice This contract is used by Donato Dapp, to allow and manage collecting funds for NGO and SME
+///@dev All function calls are currently implemented without side effects
 contract Donato is Ownable, AdminRole {
 
 	using SafeMath for uint;
@@ -32,7 +36,7 @@ contract Donato is Ownable, AdminRole {
   mapping (address => address) public receiversContractAddresses;//List of receivers contract's addresses
   mapping (string => mapping (string => bool)) private countryVATList;//Mapping of VAT numbers per country, used to avoid double subscription with same VAT number
 
-  //Events
+  ///Events
   event ApplicationReceived(address candidateAddress, string name, string VAT);
   event ReceiverContractCreated(address candidateAddress, string name, string category, string description, string country, string VAT, address receiverContractAddress);
 
@@ -43,7 +47,13 @@ contract Donato is Ownable, AdminRole {
     tokenContractAddress = _tokenContractAddress;//Save DAI contract address sent as parameter
   }
 
-
+  ///@notice Send an application to DonaTo Dapp to receive funds, the application is pending untill it's accepted or rejected
+  ///@dev Save candidate data to a struct Candidate and save sender address to the pending address array
+  ///@param _name The name of the company or association
+  ///@param _category If it is a company, an association or an NGO
+  ///@param _description Who they are and the reason why they need funds
+  ///@param _country The country where they are located
+  ///@param _VAT For identification, the fiscal code of the company or their VAT number
 	function sendApplication(string calldata _name, string calldata _category, string calldata _description, string calldata _country, string calldata _VAT) external {
   	require(bytes(_name).length != 0 && bytes(_category).length != 0 && bytes(_country).length != 0 && bytes(_VAT).length != 0, "Name, Category, Country and VAT number can't be empty");
     require(countryVATList[_country][_VAT] != true, "VAT number already used");
@@ -58,16 +68,30 @@ contract Donato is Ownable, AdminRole {
   	emit ApplicationReceived(msg.sender, _name, _VAT);
 	}
 
+  ///@notice Get all the addresses of the candidates that are waiting for approval
+  ///@dev Get all addresses contained inside pendingAddresses array, reject if array is empty
+  ///@return pending addresses array
 	function getPendingAddresses() external view onlyAdmin returns (address[] memory) {
 		require(pendingAddresses.length > 0, "No pending addresses for the moment");
 		return pendingAddresses;
 	}
 
+  ///@notice Get all the addresses of the candidates that are active
+  ///@dev Get all addresses contained inside activeAddresses array, reject if array is empty
+  ///@return active addresses array
   function getActiveAddresses() external view onlyAdmin returns (address[] memory) {
     require(activeAddresses.length > 0, "No active addresses for the moment");
     return activeAddresses;
   }  
 
+  ///@notice Get the informations of a Candidate
+  ///@dev Return candidate data contained in the struct, reject if candidate address is wrong
+  ///@param _candidateAddress The address of the required candidate
+  ///@return name The name of the candidate as a string
+  ///@return category The category of the candidate as a string
+  ///@return description The description of the candidate as a string
+  ///@return country The country of the candidate as a string
+  ///@return VAT The VAT of the candidate as a string
 	function getCandidateData(address _candidateAddress) external view onlyAdmin returns(
 		string memory name,
 		string memory category,
@@ -85,6 +109,10 @@ contract Donato is Ownable, AdminRole {
   	);
 	}
 
+  ///@notice Evaluate a candidate application, accept it or reject it
+  ///@dev Use this function to accept or reject a candidate, address of the candidate will be added to activeAddress array if accepted and delete from pendingArray if accepted or rejected  
+  ///@param _candidateAddress The address of the required candidate
+  ///@param _validation Set to true to accept application or false to reject
 	function evaluateCandidate(address payable _candidateAddress, bool _validation) external onlyAdmin{
     require(msg.sender != address(0), "Address 0 calling");
   	require(candidatesList[_candidateAddress].exists == true, "This address doesn't match with any pending candidate");
--- contracts/DonatoReceiver.sol
+++ contracts/DonatoReceiver.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.5.0;
 import "./SafeMath.sol";
 import "./ERC20Mintable.sol";//MoonPayToken Contract
 
+///@title DonatoReceiver
+///@author Lorenzo Bersano and Raphael Pinto
+///@notice This contract is used and created by Donato Dapp
+///@dev All function calls are currently implemented without side effects
 contract DonatoReceiver {
 
 	using SafeMath for uint;
@@ -17,7 +21,7 @@ contract DonatoReceiver {
   string country;
   string VAT;
 
-  //Events
+  ///Events
   event ReceiverWithdrawed(string receiverName, uint amountWithdrawed, string withdrawallReason);
 
   constructor (address payable _candidateAddress, string memory _name, string memory _category, string memory _description, string memory _country, string memory _VAT, address _tokenAddress) payable public {
@@ -32,7 +36,9 @@ contract DonatoReceiver {
     VAT = _VAT;
   }
 
-
+  ///@notice Called by the candidate when he wants to receive the funds
+  ///@dev Send token funds of the contracts to Donato Dapp for further conversion into fiat and transfer via regular fiat system
+  ///@param _withdrawalReason The reason invoked by the candidate on why he needs to withdraw the funds
   function withdrawCall(string calldata _withdrawalReason) external {
     require(msg.sender != address(0), "Address 0 calling");
   	require(msg.sender == _owner, "Caller is not the owner");
@@ -45,5 +51,4 @@ contract DonatoReceiver {
 
     emit ReceiverWithdrawed(name, tokenbalance, _withdrawalReason);
   }
-
 }
